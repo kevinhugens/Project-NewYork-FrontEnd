841c42f4d184253da6f755143ee89e750f29a8de
Arno <r0741327@student.thomasmore.be>
2020-12-07 09:57:29 +0100

    Wildcard route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { SharedModule } from './shared/shared.module';
+import { WildcardRouteComponent } from './wildcard-route/wildcard-route.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WildcardRouteComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
