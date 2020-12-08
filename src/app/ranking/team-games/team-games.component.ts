bcf4a70a3ca6b78211ef288be892922d8db99a94
woutervanaelten <r0748324@student.thomasmore.be>
2020-12-08 14:24:38 +0100

    Teams card afgwerkt

--- src/app/ranking/team-games/team-games.component.ts
+++ src/app/ranking/team-games/team-games.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Team } from '../../shared/models/team.model';
-import { Competition } from '../../shared/models/competition.model';
 import { TeamService } from '../../shared/services/team.service';
 import { Game } from '../../shared/models/game.model';
 import { GameService } from '../../shared/services/game.service';
+import { CompetitionService } from '../../shared/services/competition.service';
 
 @Component({
   selector: 'app-team-games',
@@ -13,15 +13,12 @@ import { GameService } from '../../shared/services/game.service';
 })
 export class TeamGamesComponent implements OnInit {
 
-  competition: Competition = new Competition(1, "Locatie", "Premier League");
   team1: Team;
-  team2: Team= new Team(2, "The A team", "Thomas Less", "TestAdress2", "../../../assets/teamPicture2.jpg", 1)
-  game: Game = new Game(1, "1v1", null, null, new Date("July 21, 2021 01:15:00"), "TEST", 1, 2, 1)
   games: Game[];
 
 
   id = this.route.snapshot.params['id'];
-  constructor(private route: ActivatedRoute, private router: Router, private _teamService: TeamService, private _gameService: GameService) {
+  constructor(private route: ActivatedRoute, private router: Router, private _teamService: TeamService, private _gameService: GameService, private _competitionService: CompetitionService) {
     this._teamService.getTeam(this.id).subscribe(result => {
       this.team1 = result;
     });
