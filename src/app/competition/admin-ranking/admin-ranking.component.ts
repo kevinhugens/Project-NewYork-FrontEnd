0c2ce9cff153ec028db267a7831c883a4fc638ad
woutervanaelten <r0748324@student.thomasmore.be>
2020-12-11 11:32:51 +0100

    aanmaken games

--- src/app/competition/admin-ranking/admin-ranking.component.ts
+++ src/app/competition/admin-ranking/admin-ranking.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Competition } from 'src/app/shared/models/competition.model';
+import { Game } from 'src/app/shared/models/game.model';
 import { Ranking } from 'src/app/shared/models/ranking.model';
 import { Team } from 'src/app/shared/models/team.model';
 import { CompetitionService } from 'src/app/shared/services/competition.service';
+import { GameService } from 'src/app/shared/services/game.service';
 import { RankingService } from 'src/app/shared/services/ranking.service';
 import { TeamService } from 'src/app/shared/services/team.service';
 
@@ -16,11 +18,15 @@ import { TeamService } from 'src/app/shared/services/team.service';
 export class AdminRankingComponent implements OnInit {
   rankings: Ranking[] = [];
   teams: Team[];
+  teams2: Team[];
   rankLength: number;
   competition: Competition;
+  game: Game = new Game(0, "", 0, 0, new Date(), 0, 0, 0, 0);
+  games: Game[];
 
   id = this.route.snapshot.params['id'];
-  constructor(private router: Router, private _rankingService: RankingService, private _teamService: TeamService, private route: ActivatedRoute, private _competitionService: CompetitionService) {
+  constructor(private router: Router, private _rankingService: RankingService, private _teamService: TeamService, private route: ActivatedRoute,
+    private _competitionService: CompetitionService, private _gameService: GameService) {
     this._rankingService.getRankings().subscribe(result => {
       result.map(res => {
         if (res.competitionID == this.id) {
@@ -35,10 +41,17 @@ export class AdminRankingComponent implements OnInit {
     });
     this._teamService.getTeams().subscribe(result => {
       this.teams = result;
+      this.teams2 = result;
     });
-    this._competitionService.getCompetition(this.id).subscribe(result=>{
+    this._teamService.getTeams().subscribe(result => {
+      this.teams2 = result;
+    });
+    this._competitionService.getCompetition(this.id).subscribe(result => {
       this.competition = result;
     });
+    this._gameService.getGames().subscribe(result=>{
+      this.games = result;
+    })
   }
 
   ngOnInit(): void {
@@ -47,5 +60,25 @@ export class AdminRankingComponent implements OnInit {
   onAdd(competition: Competition) {
     this._rankingService.selectedCompetion = competition;
     this.router.navigate(['addTeam']);
+  }
+
+  makeGames(competition: Competition) {
+    for (let i = 0; i < this.teams2.length; i += 0) {
+      for (let j = 0; j < this.teams2.length; j++) {
+        if (j + 1 < this.teams2.length) {
+          console.log(this.teams2[i].teamID + "vs" + this.teams2[j + 1].teamID);
+          this.game.team1ID = this.teams2[i].teamID;
+          this.game.team2ID = this.teams2[j+1].teamID;
+          this.game.competitionID = competition.competitionID;
+          this._gameService.addGame(this.game).subscribe(()=>{
+            this._gameService.getGames().subscribe(result=>{
+              this.games = result;
+            })
+          });
+          
+        }
+      }
+      this.teams2 = this.teams2.slice(1);
+    }
   }
 }
